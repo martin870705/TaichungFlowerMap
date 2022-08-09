209fab382404037a8587360aa9013c58d7736186
cielforgithub <60776436+cieliscute@users.noreply.github.com>
2022-08-09 16:35:20 +0800

    20220809class
    map測試mask,translate等等

---
+++ 05-參考資料/凱崴/程式/maptest/maptest_finalver.js
@@ -0,0 +1,12 @@
+const taichungtest=document.getElementById('taichungtestimg');
+console.dir(taichungtest);
+function imgchange(){
+    // taichungtest.href.animVal="https://shoplineimg.com/5a3ce75f59d52417c8001122/622e120327f356002931d425/750x.jpg?";
+    // taichungtest.href.baseVal="https://shoplineimg.com/5a3ce75f59d52417c8001122/622e120327f356002931d425/750x.jpg?";
+    // taichungtest.style.transform="scale(2)";
+}
+function imgchangeback(){
+    // taichungtest.href.animVal="./svg_work/shortfocal-sakura-028-620x413.jpg"
+    // taichungtest.href.baseVal="./svg_work/shortfocal-sakura-028-620x413.jpg"
+    taichungtest.style.transform="";
+}
