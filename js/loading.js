e630318ebe2e63057320080d4cfe67f9bea2781a
martin870705 <107013151+martin870705@users.noreply.github.com>
2022-08-23 02:29:35 +0800

    loading畫面

---
+++ js/loading.js
@@ -0,0 +1,33 @@
+// document.body.style.position = 'fixed'
+document.body.style.overflowY = 'hidden'
+window.onload = function () {
+    const test = document.querySelector('#loading')
+    test.style.opacity = '0'
+    setTimeout(function () {
+        test.style.display = 'none'
+        // document.body.style.position = 'unset'
+        document.body.style.overflowY = 'scroll'
+    }, 3000)
+}
+
+       // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: true })
+    .add({
+        targets: '.ml12 .letter',
+        translateX: [40, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 500 + 30 * i
+    }).add({
+        targets: '.ml12 .letter',
+        translateX: [0, -30],
+        opacity: [1, 0],
+        easing: "easeInExpo",
+        duration: 1100,
+        delay: (el, i) => 100 + 30 * i
+    });
